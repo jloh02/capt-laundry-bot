03d13beb7f43b77de644c90335465c5b04cac792
Jonathan Loh <jonathanloh02@gmail.com>
2024-10-30 15:11:40 +0800

    Add zibo into contributors

--- src/constants.py
+++ src/constants.py
@@ -25,4 +25,4 @@ class ConvState(str, Enum):
 SELECT_COMMAND_DESCRIPTION = "Select the washer/dryer that you want to use"
 STATUS_COMMAND_DESCRIPTION = "Check the status of Washers and Dryers"
 
-WELCOME_MESSAGE = f"Welcome to CAPT Laundry Bot!\n\nUse the following commands to use this bot:\n/select: {SELECT_COMMAND_DESCRIPTION}\n/status: {STATUS_COMMAND_DESCRIPTION}\n\nThank you for using the bot!\nDeveloped by: @jloh02"
+WELCOME_MESSAGE = f"Welcome to CAPT Laundry Bot!\n\nUse the following commands to use this bot:\n/select: {SELECT_COMMAND_DESCRIPTION}\n/status: {STATUS_COMMAND_DESCRIPTION}\n\nThank you for using the bot!\nDeveloped by: @jloh02, @zozibo"
